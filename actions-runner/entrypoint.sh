f4f5791a8ba1a3f34b1fe3d72ee47513422afac0
Matthew Kobayashi <matthew@kobayashi.au>
2023-05-30 18:10:04 +1000

    Fix paths

--- actions-runner/entrypoint.sh
+++ actions-runner/entrypoint.sh
@@ -8,16 +8,14 @@ echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
 
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
-cd /home/docker/actions-runner
-
-./config.sh --url https://github.com/${REPOSITORY} --token ${REG_TOKEN}
+/actions-runner/config.sh --url https://github.com/${REPOSITORY} --token ${REG_TOKEN}
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${REG_TOKEN}
+    /actions-runner/config.sh remove --unattended --token ${REG_TOKEN}
 }
 
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 
-./run.sh & wait $!
+/actions-runner/entrypoint.sh & wait $!
