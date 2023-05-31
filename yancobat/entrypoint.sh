8b5a442058117c789b9d71020fa87aece5b95787
Matthew Kobayashi <matthew@kobayashi.au>
2023-05-31 14:59:10 +1000

    More secrets support

--- yancobat/entrypoint.sh
+++ yancobat/entrypoint.sh
@@ -4,5 +4,13 @@ if [ -f /run/secrets/GITHUB_TOKEN ]; then
 	GITHUB_TOKEN=$(</run/secrets/GITHUB_TOKEN)
 fi
 
+if [ -f /run/secrets/NOTIFY_TELEGRAM_TOKEN ]; then
+	NOTIFY_TELEGRAM_TOKEN=$(</run/secrets/NOTIFY_TELEGRAM_TOKEN)
+fi
+
+if [ -f /run/secrets/NOTIFY_SLACK_TOKEN ]; then
+	NOTIFY_SLACK_TOKEN=$(</run/secrets/NOTIFY_SLACK_TOKEN)
+fi
+
 ansible-playbook /ansible/clone-repo.yaml
 exec supercronic /ansible/crontab/ansible-cron
