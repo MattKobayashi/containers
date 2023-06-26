3188aba488e01a9c020af4f5951f3b06fa681370
Matthew Kobayashi <matthew@kobayashi.au>
2023-06-26 13:37:00 +1000

    Disable automatic updates

--- actions-runner/entrypoint.sh
+++ actions-runner/entrypoint.sh
@@ -12,7 +12,7 @@ echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
 
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
-/actions-runner/config.sh --url https://github.com/${REPOSITORY} --token ${REG_TOKEN}
+/actions-runner/config.sh --url https://github.com/${REPOSITORY} --token ${REG_TOKEN} --disableupdate
 
 cleanup() {
     echo "Removing runner..."
