37f948544e0edc9c37bdd52032f6447b718e43b6
Matthew Kobayashi <matthew@kobayashi.au>
2023-06-27 12:57:02 +1000

    Add certbot-cloudflare

---
+++ certbot-cloudflare/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cat > cli.ini << EOF
+dns-cloudflare = True
+agree-tos = True
+no-eff-email = True
+keep-until-expiring = True
+dns-cloudflare-propagation-seconds = 30
+dns-cloudflare-credentials = /run/secrets/CERTBOT_CF_DNS_API_TOKEN
+deploy-hook = cp -RL /etc/letsencrypt/live/$DOMAIN/ /opt/certs/ && chmod -R o+r /opt/certs/
+domain = $DOMAIN
+email = $EMAIL
+EOF
+
+exec supercronic /crontab/certbot-cron
