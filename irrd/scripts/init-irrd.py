59819f4bbc88ffdc5c24737e63b86586a7c1e340
Matthew Kobayashi <1856537+MattKobayashi@users.noreply.github.com>
2024-10-09 07:56:53 +1000

    fix: Update `irrexplorer` to latest upstream (#319)

--- irrd/scripts/init-irrd.py
+++ irrd/scripts/init-irrd.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
-import psycopg2
-import time
-import os
-import yaml
+"""Script to initialize the IRRd database."""
 
-def execute_sql_command(host, admin_database, admin_user, admin_password, sql_command, max_retries=10, retry_interval=5):
+import time
+import sys
+
+import psycopg2  # type: ignore
+import yaml  # type: ignore
+
+
+def execute_sql_command(
+        db_host,
+        db_admin_database,
+        db_admin_user,
+        db_admin_password,
+        db_sql_command,
+        max_retries=10,
+        retry_interval=5
+        ):
     """Executes an SQL command (like CREATE DATABASE) on a PostgreSQL server,
        waiting for the database to be ready.
 
     Args:
         host: Hostname or IP of the PostgreSQL server.
         admin_database: Name of an existing database for the initial connection
-                        (typically 'postgres' or a similar administrative database).
+            (typically 'postgres' or a similar administrative database).
         admin_user: Username with sufficient privileges to create databases.
         admin_password: Password for the admin_user.
         sql_command: The SQL command to execute.
@@ -27,49 +39,68 @@ def execute_sql_command(host, admin_database, admin_user, admin_password, sql_co
         try:
             # Attempt connection to the admin database
             conn = psycopg2.connect(
-                host=host,
-                database=admin_database,
-                user=admin_user,
-                password=admin_password
+                host=db_host,
+                database=db_admin_database,
+                user=db_admin_user,
+                password=db_admin_password
             )
             conn.set_session(autocommit=True)
             cur = conn.cursor()
 
             # Execute the SQL command
-            cur.execute(sql_command)
+            cur.execute(db_sql_command)
             print("SQL command executed successfully.")
             return True
 
-        except psycopg2.OperationalError as e:
-            print(f"Database not ready yet. Retrying in {retry_interval} seconds... (Attempt {retries + 1}/{max_retries})")
+        except psycopg2.OperationalError:
+            print(
+                f"Database not ready yet. Retrying in {retry_interval} seconds... "
+                f"(Attempt {retries + 1}/{max_retries})"
+            )
             time.sleep(retry_interval)  # Wait before retrying
             retries += 1
 
-        except psycopg2.errors.DuplicateDatabase as e:
+        except psycopg2.errors.DuplicateDatabase:
             print("Database already exists, skipping...")
             return True
 
-        except psycopg2.errors.DuplicateObject as e:
+        except psycopg2.errors.DuplicateObject:
             print("Object already exists, skipping...")
             return True
 
     # Max retries reached
-    print("Error: Database connection could not be established after multiple retries.")
+    print(
+        "Error: Database connection could not be established "
+        "after multiple retries."
+    )
     return False
 
-with open("/etc/irrd.yaml", "r") as yaml_conf:
-    irrd_conf = yaml.safe_load(yaml_conf)
+
+try:
+    with open("/etc/irrd.yaml", "r", encoding="utf-8") as yaml_conf:
+        irrd_conf = yaml.safe_load(yaml_conf)
+except FileNotFoundError:
+    print("Error: Could not find the configuration file at /etc/irrd.yaml.")
+    sys.exit(1)
 
 host = irrd_conf["irrd"]["database_url"].split("/")[2].split("@")[1]
 admin_database = irrd_conf["irrd"]["database_url"].split("/")[3]
-admin_user = irrd_conf["irrd"]["database_url"].split("/")[2].split("@")[0].split(":")[0]
-admin_password = irrd_conf["irrd"]["database_url"].split("/")[2].split("@")[0].split(":")[1]
+db_url = irrd_conf["irrd"]["database_url"].split("/")
+db_credentials = db_url[2].split("@")[0].split(":")
+admin_user = db_credentials[0]
+admin_password = db_credentials[1]
 
 # Create the database first
 sql_command = f"""
 CREATE DATABASE {admin_database};
 """
-execute_sql_command(host, admin_database, admin_user, admin_password, sql_command)
+execute_sql_command(
+    host,
+    admin_database,
+    admin_user,
+    admin_password,
+    sql_command
+    )
 
 # Do the rest
 sql_command = f"""
@@ -78,4 +109,10 @@ GRANT ALL PRIVILEGES ON DATABASE {admin_database} TO {admin_user};
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 GRANT ALL ON SCHEMA public TO {admin_user};
 """
-execute_sql_command(host, admin_database, admin_user, admin_password, sql_command)
+execute_sql_command(
+    host,
+    admin_database,
+    admin_user,
+    admin_password,
+    sql_command
+    )
--- irrd/scripts/svc-irrexplorer.py
+++ irrd/scripts/svc-irrexplorer.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
-import uvicorn
+"""Script to start the IRR Explorer web application."""
+
+import uvicorn  # type: ignore
 
 if __name__ == "__main__":
-	uvicorn.run("irrexplorer.app:app", host="0.0.0.0", port=8000, workers=4, log_level="info")
+    uvicorn.run(
+        "irrexplorer.app:app",
+        host="0.0.0.0",
+        port=8000,
+        workers=4,
+        log_level="info"
+        )
