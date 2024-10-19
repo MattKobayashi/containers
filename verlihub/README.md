50e180a6814f94bd0ce0e9a4191f1df2023dafd6
Matthew Kobayashi <1856537+MattKobayashi@users.noreply.github.com>
2024-10-19 15:15:23 +1000

    fix: Prefix all env vars and secrets (#325)

--- verlihub/README.md
+++ verlihub/README.md
@@ -14,21 +14,21 @@ This image is configured using environment variables and Docker secrets.
 
 |Variable name|Required?|Description|
 |---|---|---|
-|`MYSQL_DB_HOST`|✅|The hostname or IP address of the MySQL/MariaDB server|
-|`MYSQL_DB_NAME`|❌|The name of the MySQL/MariaDB database to be created (defaults to `verlihub`)|
-|`MYSQL_USER`|❌|The username of the database user with full permissions over the `MYSQL_DB_NAME` database (defaults to `verlihub`)|
-|`VH_CONFIG_DIR`|❌|The directory where Verlihub stores configuration data (defaults to `/opt/verlihub/.config/verlihub`)|
+|`VH_MYSQL_DB_HOST`|✅|The hostname or IP address of the MySQL/MariaDB server|
+|`VH_MYSQL_DB_NAME`|❌|The name of the MySQL/MariaDB database to be created (defaults to `verlihub`)|
+|`VH_MYSQL_USER`|❌|The username of the database user with full permissions over the `MYSQL_DB_NAME` database (defaults to `verlihub`)|
+|`VH_HUB_CONFIG_DIR`|❌|The directory where Verlihub stores configuration data (defaults to `/opt/verlihub/.config/verlihub`)|
 |`VH_HUB_HOST`|✅|The hostname of the Verlihub server|
 |`VH_HUB_PORT`|❌|The port that the Verlihub server runs on (defaults to `411` when `NET_BIND_SERVICE` capability is added, otherwise `4111`)|
 |`VH_HUB_NAME`|❌|The name of the Verlihub server (defaults to `Verlihub`)|
-|`VH_MASTER_NAME`|✅|The username of the master user on the Verlihub server|
+|`VH_HUB_MASTER_NAME`|✅|The username of the master user on the Verlihub server|
 
 ### Secrets
 
 |Secret name|Required?|Description|
 |---|---|---|
-|`MYSQL_PASSWORD`|✅|The password of the database user with full permissions over the `MYSQL_DB_NAME` database|
-|`VH_MASTER_PASSWORD`|✅|The password of the master user on the Verlihub server|
+|`VH_MYSQL_PASSWORD`|✅|The password of the database user with full permissions over the `MYSQL_DB_NAME` database|
+|`VH_HUB_MASTER_PASSWORD`|✅|The password of the master user on the Verlihub server|
 
 ## Running the container
 
--- verlihub/scripts/setup.py
+++ verlihub/scripts/setup.py
@@ -39,10 +39,10 @@ def execute_query(query_connection, query):
 def generate_config_file(file_path):
     """Generates a configuration file with predefined contents."""
     config_content = f"""
-db_host = {MYSQL_DB_HOST}
-db_data = {MYSQL_DB_NAME}
-db_user = {MYSQL_USER}
-db_pass = {MYSQL_PASSWORD}
+db_host = {VH_MYSQL_DB_HOST}
+db_data = {VH_MYSQL_DB_NAME}
+db_user = {VH_MYSQL_USER}
+db_pass = {VH_MYSQL_PASSWORD}
 db_charset = utf8mb4
         """
     try:
@@ -55,31 +55,31 @@ db_charset = utf8mb4
 
 # Get the configuration from environment variables and secrets
 try:
-    MYSQL_DB_HOST = os.environ['MYSQL_DB_HOST']
-    MYSQL_DB_NAME = os.environ.get('MYSQL_DB_NAME', 'verlihub')
-    MYSQL_USER = os.environ.get('MYSQL_USER', 'verlihub')
+    VH_MYSQL_DB_HOST = os.environ['MYSQL_DB_HOST']
+    VH_MYSQL_DB_NAME = os.environ.get('MYSQL_DB_NAME', 'verlihub')
+    VH_MYSQL_USER = os.environ.get('MYSQL_USER', 'verlihub')
     try:
         with open(
-            '/run/secrets/MYSQL_PASSWORD', 'r', encoding='utf-8'
+            '/run/secrets/VH_MYSQL_PASSWORD', 'r', encoding='utf-8'
         ) as file:
-            MYSQL_PASSWORD = file.read().strip()
+            VH_MYSQL_PASSWORD = file.read().strip()
     except FileNotFoundError:
-        print("MYSQL_PASSWORD secret not found.")
+        print("VH_MYSQL_PASSWORD secret not found.")
         sys.exit(1)
-    VH_CONFIG_DIR = os.environ.get(
-        'VH_CONFIG_DIR', '/opt/verlihub/.config/verlihub'
+    VH_HUB_CONFIG_DIR = os.environ.get(
+        'VH_HUB_CONFIG_DIR', '/opt/verlihub/.config/verlihub'
     ).rstrip('/')
     VH_HUB_HOST = os.environ['VH_HUB_HOST']
     VH_HUB_PORT = os.environ.get('VH_HUB_PORT', '411')
     VH_HUB_NAME = os.environ.get('VH_HUB_NAME', 'Verlihub')
-    VH_MASTER_NAME = os.environ['VH_MASTER_NAME']
+    VH_HUB_MASTER_NAME = os.environ['VH_HUB_MASTER_NAME']
     try:
         with open(
-            '/run/secrets/VH_MASTER_PASSWORD', 'r', encoding='utf-8'
+            '/run/secrets/VH_HUB_MASTER_PASSWORD', 'r', encoding='utf-8'
         ) as file:
-            VH_MASTER_PASSWORD = file.read().strip()
+            VH_HUB_MASTER_PASSWORD = file.read().strip()
     except FileNotFoundError:
-        print("VH_MASTER_PASSWORD secret not found.")
+        print("VH_HUB_MASTER_PASSWORD secret not found.")
         sys.exit(1)
 except KeyError:
     print("One or more environment variables not set.")
@@ -87,7 +87,7 @@ except KeyError:
 
 # Connect to the MySQL database
 connection = create_connection(
-    MYSQL_DB_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB_NAME
+    VH_MYSQL_DB_HOST, VH_MYSQL_USER, VH_MYSQL_PASSWORD, VH_MYSQL_DB_NAME
 )
 
 # Generate the configuration file
@@ -95,7 +95,7 @@ generate_config_file(VH_CONFIG_DIR + "/dbconfig")
 
 # Define the configuration queries
 config_queries = [
-    f"CREATE TABLE IF NOT EXISTS {MYSQL_DB_NAME}.SetupList (file varchar(30), var varchar(50), val text, primary key(file, var));",
+    f"CREATE TABLE IF NOT EXISTS {VH_MYSQL_DB_NAME}.SetupList (file varchar(30), var varchar(50), val text, primary key(file, var));",
     f"UPDATE SetupList SET val='{VH_HUB_HOST}:{VH_HUB_PORT}' WHERE var='hub_host';",
     f"UPDATE SetupList SET val='{VH_HUB_PORT}' WHERE var='listen_port';"
 ]
@@ -106,7 +106,7 @@ for query in config_queries:
 
 # Create the master user
 master_query = f"""
-INSERT INTO reglist (nick, class, login_pwd, pwd_change, pwd_crypt) VALUES ('{VH_MASTER_NAME}', 10, '{VH_MASTER_PASSWORD}', 0, 0);
+INSERT INTO reglist (nick, class, login_pwd, pwd_change, pwd_crypt) VALUES ('{VH_HUB_MASTER_NAME}', 10, '{VH_HUB_MASTER_PASSWORD}', 0, 0);
 """
 execute_query(connection, master_query)
 
@@ -129,6 +129,6 @@ except Error as e:
 # Export the configuration directory as an environment variable
 try:
     with open('/opt/verlihub/verlihub.env', 'w', encoding='utf-8') as file:
-        file.write(f"VERLIHUB_CFG={VH_CONFIG_DIR}")
+        file.write(f"VERLIHUB_CFG={VH_HUB_CONFIG_DIR}")
 except IOError as e:
     print(f"An error occurred while writing to the file: {e}")
