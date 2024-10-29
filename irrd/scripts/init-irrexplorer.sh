f83a19ecc8b544786759d564e6660f12b7fc50b9
Matthew Kobayashi <matthew@kobayashi.au>
2024-10-29 20:22:29 +1000

    fix(irrd): Run poetry install during image build

--- irrd/scripts/init-irrexplorer.sh
+++ irrd/scripts/init-irrexplorer.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 cd /opt/irrexplorer/
-/opt/irrexplorer/bin/poetry lock
-/opt/irrexplorer/bin/poetry install
-/opt/irrexplorer/bin/poetry add psycopg2@^2.9.9
 /opt/irrexplorer/bin/poetry run python3 init-irrexplorer.py
 /opt/irrexplorer/bin/poetry run frontend-install
 /opt/irrexplorer/bin/poetry run frontend-build
